e5ae4175eb956c36812d838c28279420dd407a60
AlineCantalice <alinedecantalice@gmail.com>
2022-05-08 15:14:43 -0300

    refatoração component stories

--- src/components/Corpo.js
+++ src/components/Corpo.js
@@ -3,10 +3,13 @@ import Sidebar from "./Sidebar";
 import Stories from "./Stories";
 
 export default function Corpo() {
+
+    const srcs = ['9gag', 'meowed', 'barked', 'nathanwpylestrangeplanet', 'wawawicomics', 'respondeai', 'filomoderna', 'memeriagourmet'];
+
     return (
         <div class="corpo">
             <div class="esquerda">
-                <Stories />
+                <Stories srcs={srcs}/>
                 <Posts />
             </div>
             <Sidebar />
--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,81 +1,27 @@
-export default function Stories() {
+import Icons from "./Icons";
+
+export default function Stories({ srcs }) {
+
+    function Story({ src }) {
+        return (
+            <div class="story">
+                <div class="imagem">
+                    <img src={`assets/img/${src}.svg`} />
+                </div>
+                <div class="usuario">
+                    {src}
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div class="stories">
-            <div class="story">
-                <div class="imagem">
-                    <img src="assets/img/9gag.svg" />
-                </div>
-                <div class="usuario">
-                    9gag
-                </div>
-            </div>
 
-            <div class="story">
-                <div class="imagem">
-                    <img src="assets/img/meowed.svg" />
-                </div>
-                <div class="usuario">
-                    meowed
-                </div>
-            </div>
+            {srcs.map(s => <Story src={s} />)}
 
-            <div class="story">
-                <div class="imagem">
-                    <img src="assets/img/barked.svg" />
-                </div>
-                <div class="usuario">
-                    barked
-                </div>
-            </div>
+            <Icons style="setinha" icons={["chevron-forward-circle"]} />
 
-            <div class="story">
-                <div class="imagem">
-                    <img src="assets/img/nathanwpylestrangeplanet.svg" />
-                </div>
-                <div class="usuario">
-                    nathanwpylestrangeplanet
-                </div>
-            </div>
-
-            <div class="story">
-                <div class="imagem">
-                    <img src="assets/img/wawawicomics.svg" />
-                </div>
-                <div class="usuario">
-                    wawawicomics
-                </div>
-            </div>
-
-            <div class="story">
-                <div class="imagem">
-                    <img src="assets/img/respondeai.svg" />
-                </div>
-                <div class="usuario">
-                    respondeai
-                </div>
-            </div>
-
-            <div class="story">
-                <div class="imagem">
-                    <img src="assets/img/filomoderna.svg" />
-                </div>
-                <div class="usuario">
-                    filomoderna
-                </div>
-            </div>
-
-            <div class="story">
-                <div class="imagem">
-                    <img src="assets/img/memeriagourmet.svg" />
-                </div>
-                <div class="usuario">
-                    memeriagourmet
-                </div>
-            </div>
-
-            <div class="setinha">
-                <ion-icon name="chevron-forward-circle"></ion-icon>
-            </div>
         </div>
     );
 }
